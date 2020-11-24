e14f82634d81dc7cbd435a64a992417eb3c72615
milan978gh <amilandev78@gmail.com>
2020-11-24 18:18:07 +0100

    update

--- js/main.js
+++ js/main.js
@@ -6,7 +6,9 @@ const menu = [
         category: "breakfast",
         price: 15.99,
         img: "./images/item-1.jpeg",
-        desc: `I'm baby woke mlkshk wolf bitters live-edge blue bottle, hammock freegan copper mug whatever cold-pressed `,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia. `,
     },
     {
         id: 2,
@@ -14,7 +16,9 @@ const menu = [
         category: "lunch",
         price: 13.99,
         img: "./images/item-2.jpeg",
-        desc: `vaporware iPhone mumblecore selvage raw denim slow-carb leggings gochujang helvetica man braid jianbing. Marfa thundercats `,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 3,
@@ -22,7 +26,9 @@ const menu = [
         category: "shakes",
         price: 6.99,
         img: "./images/item-3.jpeg",
-        desc: `ombucha chillwave fanny pack 3 wolf moon street art photo booth before they sold out organic viral.`,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 4,
@@ -30,7 +36,9 @@ const menu = [
         category: "breakfast",
         price: 20.99,
         img: "./images/item-4.jpeg",
-        desc: `Shabby chic keffiyeh neutra snackwave pork belly shoreditch. Prism austin mlkshk truffaut, `,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 5,
@@ -38,7 +46,9 @@ const menu = [
         category: "lunch",
         price: 22.99,
         img: "./images/item-5.jpeg",
-        desc: `franzen vegan pabst bicycle rights kickstarter pinterest meditation farm-to-table 90's pop-up `,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 6,
@@ -46,7 +56,9 @@ const menu = [
         category: "shakes",
         price: 18.99,
         img: "./images/item-6.jpeg",
-        desc: `Portland chicharrones ethical edison bulb, palo santo craft beer chia heirloom iPhone everyday`,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 7,
@@ -54,7 +66,9 @@ const menu = [
         category: "breakfast",
         price: 8.99,
         img: "./images/item-7.jpeg",
-        desc: `carry jianbing normcore freegan. Viral single-origin coffee live-edge, pork belly cloud bread iceland put a bird `,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 8,
@@ -62,7 +76,9 @@ const menu = [
         category: "lunch",
         price: 12.99,
         img: "./images/item-8.jpeg",
-        desc: `on it tumblr kickstarter thundercats migas everyday carry squid palo santo leggings. Food truck truffaut  `,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 9,
@@ -70,7 +86,9 @@ const menu = [
         category: "shakes",
         price: 16.99,
         img: "./images/item-9.jpeg",
-        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     },
     {
         id: 10,
@@ -78,7 +96,9 @@ const menu = [
         category: "dinner",
         price: 39.99,
         img: "./images/item-10.jpeg",
-        desc: `skateboard fam synth authentic semiotics. Live-edge lyft af, edison bulb yuccie crucifix microdosing.`,
+        desc: `Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        Repudiandae, sint quam. Et reprehenderit fugiat nesciunt inventore
+        laboriosam excepturi! Quo, officia.`,
     }
 ];
 const sectionCenter = document.querySelector('.section-center');
